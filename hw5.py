a63fff75849cad8e75e5b9ea3294579b3bb78b17
caron2002 <qkrtkddnr122@gmail.com>
2024-04-13 20:21:57 +0900

    Add hw5

---
+++ hw5.py
@@ -0,0 +1,29 @@
+def read_single_digit(num):
+  if num == '1':
+    return '일'
+  elif num == '2':
+    return '이'
+  elif num == '3':
+    return '삼'
+  elif num == '4':
+    return '사'
+  elif num == '5':
+    return '오'
+  elif num == '6':
+    return '육'
+  elif num == '7':
+    return '칠'
+  elif num == '8':
+    return '팔'
+  elif num == '9':
+    return '구'
+
+def red_number(num):
+  num1 = read_single_digit(num[0])
+  num2 = read_single_digit(num[1])
+  num3 = read_single_digit(num[2])
+
+  return f'{num1} {num2} {num3}'
+
+num = input('세 자리 정수 입력: ')
+print(red_number(num))
